772b84940219ad8096362cf181f6ad779f18faa5
janainaacm <janaina@Janainas-Air.lan>
2023-09-20 11:53:48 +0200

    Third commit

---
+++ src/com/Janaina/demo/GitHubTest.java
@@ -0,0 +1,10 @@
+package com.Janaina.demo;
+
+public class GitHubTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is happening in another branch!");
+    }
+
+}
