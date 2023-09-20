ac861df3b18757e06db726bfcb878f2aa4e53745
janainaacm <janaina@Janainas-Air.lan>
2023-09-20 11:51:36 +0200

    Second commit

--- src/com/Janaina/demo/Main.java
+++ src/com/Janaina/demo/Main.java
@@ -1,2 +1,10 @@
-package com.Janaina.demo;public class Main {
+package com.Janaina.demo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("HELLO WORLD!");
+
+    }
 }
